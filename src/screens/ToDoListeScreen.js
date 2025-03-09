cac8111ddb1c18a7e83661e2ea12019ef47f5ad1
MateoMBR <m.burnichon.pro@gmail.com>
2025-03-09 15:26:51 +0100

    To do List Step 2

--- src/screens/ToDoListeScreen.js
+++ src/screens/ToDoListeScreen.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 function ToDoListeScreen() {
+  const [goal, setGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const addGoalHandler = () => {
+    setGoals(currentGoals => [...currentGoals, goal]);
+    setGoal('');
+  };
+
   return (
-    <View>
-      <Text>ToDoListe</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Nouvel objectif"
+          style={styles.input}
+          onChangeText={setGoal}
+          value={goal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
+      </View>
+      <View>
+        {goals.map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+});
 
 export default ToDoListeScreen;
