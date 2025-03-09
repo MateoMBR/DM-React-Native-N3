2015320d468f81b183ec06cf5411a30cdd13288d
MateoMBR <m.burnichon.pro@gmail.com>
2025-03-09 15:44:30 +0100

    Wether APP Step 2
    Get Location

--- src/screens/WeatherAppScreen.js
+++ src/screens/WeatherAppScreen.js
@@ -1,12 +1,44 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 function WeatherAppScreen() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
-    <View>
-      <Text>WeatherApp</Text>
+    <View style={styles.container}>
+      <Text>{text}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default WeatherAppScreen;
