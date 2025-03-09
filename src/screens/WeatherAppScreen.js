53cad9be9dddbf371a0589e14ea2efa3b8899f51
MateoMBR <m.burnichon.pro@gmail.com>
2025-03-09 15:46:00 +0100

    Wether APP Step 3
    Get Wether direct in my location

--- src/screens/WeatherAppScreen.js
+++ src/screens/WeatherAppScreen.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import * as Location from 'expo-location';
+import axios from 'axios';
+
+const API_KEY = '7e8e2e58051fba9a97cdd779cb4910c6';
 
 function WeatherAppScreen() {
   const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
@@ -16,19 +20,40 @@ function WeatherAppScreen() {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
+
+      const { latitude, longitude } = location.coords;
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+      );
+      setWeather(response.data);
     })();
   }, []);
 
-  let text = 'Waiting..';
   if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
+    return (
+      <View style={styles.container}>
+        <Text>{errorMsg}</Text>
+      </View>
+    );
+  }
+
+  if (!weather) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
   }
 
   return (
     <View style={styles.container}>
-      <Text>{text}</Text>
+      <Text>{weather.name}</Text>
+      <Text>{weather.main.temp} °C</Text>
+      <Text>{weather.weather[0].description}</Text>
+      <Image
+        style={styles.icon}
+        source={{ uri: `http://openweathermap.org/img/wn/${weather.weather[0].icon}.png` }}
+      />
     </View>
   );
 }
@@ -38,6 +63,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  icon: {
+    width: 50,
+    height: 50,
   },
 });
 
